d67cddcb1e7283293fd4013c5221a7b0fdea3e31
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-06-01 05:10:21 +0100

    This commit will push all tasks

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,8 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((successCall, failureCall) => {
+    if (success) {
+      successCall({ status: 200, body: 'Success' });
+    } else {
+      failureCall(new Error('The fake API is not working currently'));
+    }
+  });}
